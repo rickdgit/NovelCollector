2bb8fbe13ee5ebc3b1e1d03bb4a86a3a13bc408e
rickdgit <rick.d@outlook.com>
2016-07-30 22:13:43 -0400

    Update part Sql OP query

--- sqlOps.py
+++ sqlOps.py
@@ -25,30 +25,69 @@ class sqlOperation(object):
         except :
             self.dbInit()
     #CRUD
-    def insertElement(self,tbName,attr,values):
-        pass
-    def updateElement(self):
+    def insertElement(self,tbName,dic):
+        value = keys = ""
+        for k,v in dic.iteritems():
+            value += k+","
+            keys += v+","
+        value = value[:-1]
+        keys = keys[:-1]
+        query = "insert into "+tbName+"("+value+") values ("+keys+");"
+        print(query)
+        #  self.cursor.execute(query)
+
+    def updateElement(self,tbName,dic):
         pass
     def deleteElement(self):
         pass
+    def retrieveMultiElement(self,tbName,condition):
+        query = "select * from "+tbName+"where "+ condition
+        print(query)
+        #  self.cursor.execute(query)
     def retrieveElement(self):
         pass
     #OPs
-    def addBook(self):
-        pass
-    def addChars(self):
-        pass
-    def addWebsetPerf(self):
-        pass
+    def addBook(self,bkName,author,indexLink,totalCharNum):
+        dic = {'bkName':bkName,'author':author,'indexLink':indexLink,'totalCharNum':totalCharNum}
+        insertElement('books',dic)
+
+
+    def addChars(self,bookID,CharTitle,CharNum,CharContect):
+        dic = {'bookID':bookID,'CharNum':CharNum,'CharTitle':CharTitle,'CharContect':CharContect}
+        insertElement('CharactersTable',dic)
+
+
+    def addWebsetPerf(self,websiteAddr,charOrder,POST,encoding,removeTags,textXPathKey,titleXPathKey,indexXPathKey,nextCharXPathKey,searchXpath):
+        dic = {'websiteAddr':websiteAddr,'charOrder':charOrder,'POST':POST,'encoding':encoding,'removeTags':removeTags,'textXPathKey':textXPathKey,'titleXPathKey':titleXPathKey,'indexXPathKey':indexXPathKey,'nextCharXPathKey':nextCharXPathKey,'searchXpath':searchXpath}
+        insertElement('websitePerf',dic)
+
+
     def addTask(self):
-        pass
+        #select the task periority
+        dic = {'bookID':bookID,'uid':uid,'bkName':bkName,'IndexLink':IndexLink,'currentChar':currentChar,'startChar':startChar,'endChar':endChar}
+        insertElement('user',dic)
+
+
     def getMultiChars(self,bookid,staratChar,endChar):
-        pass
+        condition = "bookID = "+bookID+ " CharNum <= "+endChar + "AND CharNum > "+startatChar 
+        retrieveElement('CharactersTable',condition)
+
+
     def getSingleChars(self):
         pass
     def getBook(self):
         pass
     def getTask(self):
+        pass
+    def updateBook(self,attr,values):
+        pass
+    def updateOneUserReadingChar(self,bookID,uid,endChar):
+        #Update read chars for only one user
+        dic = {}
+        updateElement('readingProc',dic)
+        pass
+    def updateUsersReadingChar(self,bookID,endChar):
+        #Used for update all user for a book's reading stage for one book
         pass
     #  def deleteBook(self):
     #      pass
